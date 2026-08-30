63223050010b76adb07b7051163abccec0d1d851
imoonmahmud <imoonmahmud@gmail.com>
2026-08-30 22:24:03 +0600

    added numpy array creation functions

--- numpy/main.py
+++ numpy/main.py
@@ -6,3 +6,14 @@ zeros = np.zeros((3, 4))
 ones = np.ones((2, 3), dtype=int)
 full = np.full((3, 3), 3.1416)
 
+# Ranges
+arange = np.arange(0, 20, 2)
+linspace = np.linspace(0, 100, 11)
+
+# Identity matrix
+eys = np.eye(4)
+
+# Empty (uninitialized - fast)
+empty = np.empty(5)
+for i in range(5):
+    empty[i] = i ** 2
