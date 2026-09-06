92770c63f355d44c4c88de66eb574ee2cd88f693
imoonmahmud <imoonmahmud@gmail.com>
2026-09-07 01:50:05 +0600

    started learning numpy mathematical functions

---
+++ mathematical_functions/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Universal Functions (ufuncs)
+arr = np.array([1, 4, 9, 16, 25])
+
+# Basic math
+print(np.sqrt(arr))
+print(np.square(arr))
+print(np.abs([-4, -2, 4, -4.6, 5.6]))
+print(np.power(arr, 3))
+
+# Rounding
